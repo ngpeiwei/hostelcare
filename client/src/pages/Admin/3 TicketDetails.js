fafae6bc1682ba71518740a17275814b5edc82e5
aimiashri <aimiashri13@gmail.com>
2025-11-20 23:13:16 +0800

    + in progress, +category correct

--- client/src/pages/Admin/3 TicketDetails.js
+++ client/src/pages/Admin/3 TicketDetails.js
@@ -132,7 +132,7 @@ const TicketDetails = () => {
   }
 
   const staffOptions = ['Nazrul Hakim', 'Muqtadir Syabil', 'Siti Aminah', 'Mohd Razif'];
-  const statusOptions = ['Open', 'Pending', 'Resolved'];
+  const statusOptions = ['New', 'Pending', 'In Progress', 'Resolved'];
 
   return (
     <div className="ticket-details-page">
@@ -278,7 +278,7 @@ const TicketDetails = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                Location
+                Hostel
               </label>
               <input
                 type="text"
@@ -291,14 +291,14 @@ const TicketDetails = () => {
             <div className="form-group">
               <label className="form-label">
                 <svg className="form-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                Building
+                Phone Number
               </label>
               <input
                 type="text"
                 className="form-input"
-                value={ticket.building}
+                value={"+60102355511"}
                 disabled
               />
             </div>
@@ -309,12 +309,12 @@ const TicketDetails = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                Floor and Room Number
+                Building and Room Number
               </label>
               <input
                 type="text"
                 className="form-input"
-                value={ticket.floorAndRoom}
+                value={"M04-09-12A"}
                 disabled
               />
             </div>
